6cd5e19183657b9a7cd52c661dab35386337d662
amberim <im.amberim@gmail.com>
2024-08-15 12:31:56 +0900

    feat: add getCompaines controller function

---
+++ controllers/companyController.js
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getCompanies = async (req, res) => {
+  const { keyword = "" } = req.query;
+  const { lastId, limit = 5, orderBy } = req.query;
+
+  let sortOption;
+  switch (orderBy) {
+    case "highestInvestment":
+      sortOption = { actualInvestment: "desc" };
+      break;
+    case "lowestInvestment":
+      sortOption = { actualInvestment: "asc" };
+      break;
+    case "highestRevenue":
+      sortOption = { revenue: "desc" };
+      break;
+    case "lowestRevenue":
+      sortOption = { revenue: "asc" };
+      break;
+    case "highestEmployees":
+      sortOption = { totalEmployees: "desc" };
+      break;
+    case "lowestEmployees":
+      sortOption = { totalEmployees: "asc" };
+      break;
+    default:
+      sortOption = { actualInvestment: "desc" };
+  }
+
+  const searchQuery = {
+    OR: [
+      { name: { contains: keyword } },
+      { description: { contains: keyword } },
+    ],
+  };
+
+  const companies = await prisma.company.findMany({
+    where: searchQuery,
+    orderBy: sortOption,
+    take: parseInt(limit),
+    skip: lastId ? 1 : 0,
+    cursor: lastId ? { id: lastId } : undefined,
+  });
+
+  const nextCursor =
+    companies.length === limit ? companies[companies.length - 1].id : null;
+  console.log(companies);
+
+  res.send({ nextCursor, list: companies });
+};
--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+import * as company from "../controllers/companyController.js";
+
 const app = express.Router();
+
+app.get("/", company.getCompanies);
 
 export default app;
