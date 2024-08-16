e03dbf663550fb9d62381d16d9e8817cff3ec70c
sanho <dieuaimer@gmail.com>
2024-08-16 16:53:14 +0900

    feat: Add utility for handling BigInt as string

--- controllers/investmentController.js
+++ controllers/investmentController.js
@@ -1,4 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { replaceBigIntToString } from "../utils/stringifyBigInt.js";
+import transformBigInt from "../utils/transformBigInt";
 
 const prisma = new PrismaClient();
+
+// 가장 투자 생성
+export const createInvestment = async (req, res) => {
+  const { companyId, name, amount, comment, password } = req;
+};
